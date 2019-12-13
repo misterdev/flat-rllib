2bed087677a5b7fa3affbecaf1145e219233274a
Devid Farinelli <devid.farinelli@gmail.com>
2019-12-13 13:15:21 +0100

    Adds RLLib utils

---
+++ rllib_wrapper/custom_preprocessor.py
@@ -0,0 +1,18 @@
+import numpy as np
+from ray.rllib.models.preprocessors import Preprocessor
+
+from observation_utils import normalize_observation
+
+class TreeObsPreprocessor(Preprocessor):
+    def _init_shape(self, obs_space, options):
+        self.step_memory = 2 # TODO options["custom_options"]["step_memory"]
+        self.tree_depth = 2
+        return sum([space.shape[0] for space in obs_space]),
+
+    def transform(self, obs):
+        if obs:
+            ret = normalize_observation(obs, self.tree_depth, observation_radius=10)
+        else:
+            ret = np.zeros(231)
+        
+        return ret
---
+++ rllib_wrapper/flatland_wrapper.py
@@ -0,0 +1,90 @@
+import gym
+import numpy as np
+from ray import rllib
+
+from flatland.envs.rail_env import RailEnv
+from flatland.envs.rail_generators import sparse_rail_generator
+from flatland.envs.schedule_generators import sparse_schedule_generator
+from flatland.envs.observations import TreeObsForRailEnv
+from flatland.envs.malfunction_generators import malfunction_from_params
+
+from flatland.utils.rendertools import RenderTool
+
+class FlatlandEnv(rllib.env.MultiAgentEnv):
+    def __init__(self, env_config):
+        width = 35
+        height = 35
+        self.n_agents = 2
+        self.tree_depth = 3
+
+        TreeObservation = TreeObsForRailEnv(max_depth=2)
+
+        # Use a the malfunction generator to break agents from time to time
+        stochastic_data = {
+            'malfunction_rate': 8000,  # Rate of malfunction occurence of single agent
+            'min_duration': 15,  # Minimal duration of malfunction
+            'max_duration': 50  # Max duration of malfunction
+        }
+
+        # Different agent types (trains) with different speeds.
+        speed_ration_map = {
+            1.: 0.,  # Fast passenger train
+            1. / 2.: 1.0,  # Fast freight train
+            1. / 3.: 0.0,  # Slow commuter train
+            1. / 4.: 0.0   # Slow freight train
+        }
+
+        self.env = RailEnv(
+            width=width,
+            height=height,
+            rail_generator=sparse_rail_generator(max_num_cities=3,
+                                                # Number of cities in map (where train stations are)
+                                                seed=1,  # Random seed
+                                                grid_mode=False,
+                                                max_rails_between_cities=2,
+                                                max_rails_in_city=3),
+            schedule_generator=sparse_schedule_generator(speed_ration_map),
+            number_of_agents=self.n_agents,
+            malfunction_generator_and_process_data=malfunction_from_params(stochastic_data),
+            # Malfunction data generator
+            obs_builder_object=TreeObservation
+        )
+
+        self.env_renderer = RenderTool(self.env, gl="PILSVG", )
+
+        self.action_space = gym.spaces.Discrete(5)
+        self.observation_space = np.zeros((1, 231))
+
+    def reset(self):
+        self.agents_done = []
+        obs = self.env.reset()
+        self.env_renderer.reset()
+        # print('================= RESET', obs)
+        return obs[0]
+
+    def step(self, action_dict):
+        obs, rewards, dones, infos = self.env.step(action_dict)
+        self.env_renderer.render_env(show=True, show_predictions=False, show_observations=True)
+        # return <obs>, <reward: float>, <done: bool>, <info: dict>
+
+        d = dict()
+        r = dict()
+        o = dict()
+        i = dict()
+        for a in range(len(self.env.agents)):
+            if a not in self.agents_done:
+                # data, distance, agent_data = self.env.obs_builder.split_tree(tree=np.array(obs[a]), current_depth=0)                
+                o[a] = obs[a]
+                r[a] = rewards[a]
+                d[a] = dones[a]
+                i[a] = {'mario': a}
+                if dones[a]:
+                    print('DONE', a)
+        d['__all__'] = dones['__all__']
+
+        # print('================= STEP', action_dict, d)
+        for agent, done in dones.items():
+            if done and agent != '__all__':
+                self.agents_done.append(agent)
+
+        return  o, r, d, i
