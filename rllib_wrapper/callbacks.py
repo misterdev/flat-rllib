b966b020af634f0a2c394eede82ca62609c1d276
Devid Farinelli <devid.farinelli@gmail.com>
2020-01-04 14:02:39 +0100

    print episodes/agents done stats after each training

--- rllib_wrapper/callbacks.py
+++ rllib_wrapper/callbacks.py
@@ -6,11 +6,13 @@ def on_episode_end(info):
     episode = info['episode']
     env = info['env'].envs[0]
     agents_done = len(env.agents_done)
-    episode_done = agents_done == env.n_agents
-    
+    episode_done = 0
+    if (agents_done == env.n_agents):
+        episode_done = 1
+
     episode.custom_metrics['score'] = episode.total_reward
     episode.custom_metrics['done'] = episode_done
-    episode.custom_metrics['agents_done'] = agents_done
+    episode.custom_metrics['agents_done'] = agents_done / env.n_agents
 
     completed = 'done {:>3.0f}% ({}/{})'.format(
         agents_done / env.n_agents * 100,
@@ -28,9 +30,11 @@ def on_train_result(info):
     result = info['result']
     custom_metrics = result['custom_metrics']
     print(
-    '\r\n==> Iteration {:,} - {:,} episodes -- avg score: {:.0f} -- avg length: {:.0f} \n'.format(
+    '\r\n==> Iteration {:,} - {:,} episodes -- avg score: {:.0f} -- avg length: {:.0f} -- {:>3.0f}% episodes dones -- {:>3.0f}% agents done\n'.format(
         result['training_iteration'],
         result['episodes_total'],
         custom_metrics['score_mean'],
-        result['episode_len_mean']
+        result['episode_len_mean'],
+        custom_metrics['done_mean'] * 100,
+        custom_metrics['agents_done_mean'] * 100
     ))
